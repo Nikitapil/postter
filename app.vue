3326a415e75f18623ba2460e9069612389e1b33e
Nikitapil <nikitapiliavets@gmail.com>
2023-10-09 23:12:27 +0300

    init nuxt project and add tailwindcss

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div :class="{dark: darkMode}">
+    <div class="min-h-full">
+      <div class="grid"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const darkMode = ref(true)
+</script>
