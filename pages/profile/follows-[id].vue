aac8580b0994646118fdcfb3a089c56de36b536d
Nikitapil <nikitapiliavets@gmail.com>
2023-11-19 20:50:13 +0300

    add follow pages navigation

---
+++ pages/profile/follows-[id].vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <MainSection :loading="false">
+      <template #title>
+        <div class="flex justify-around">
+          <NuxtLink
+            class="dark:text-white hover:opacity-70 default-transition"
+            active-class="border-b"
+            :to="followersLink"
+            >Followers
+          </NuxtLink>
+          <NuxtLink
+            class="dark:text-white hover:opacity-70 default-transition"
+            active-class="border-b"
+            :to="followingLink"
+          >
+            Following
+          </NuxtLink>
+        </div>
+      </template>
+
+      <NuxtPage />
+    </MainSection>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+const profileId = computed(() => route.params.id);
+
+const followersLink = computed(
+  () => `/profile/follows-${profileId.value}/followers`
+);
+const followingLink = computed(
+  () => `/profile/follows-${profileId.value}/following`
+);
+</script>
---
+++ pages/profile/follows-[id]/followers.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    Followers
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
