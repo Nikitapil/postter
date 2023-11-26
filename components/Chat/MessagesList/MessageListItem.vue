4b6e6f06d1a3c5ba107587580d146643e5a56ff4
Nikitapil <nikitapiliavets@gmail.com>
2023-11-26 21:32:36 +0300

    finish message layout

--- components/Chat/MessagesList/MessageListItem.vue
+++ components/Chat/MessagesList/MessageListItem.vue
@@ -1,17 +1,28 @@
 <template>
   <div
-    class="max-w-xs bg-gray-700 p-2 rounded-md text-white"
+    class="flex flex-col max-w-xs bg-gray-700 p-2 rounded-md text-white"
     :class="{ '!bg-blue-700': isMyMessage }"
   >
-    {{ message.text }}
+    <h3
+      class="text-xs text-blue-400 font-bold mb-2"
+      :class="{ 'text-blue-200': isMyMessage }"
+    >
+      {{ message.author.name }} @{{ message.author.username }}
+    </h3>
+    <p>{{ message.text }}</p>
+    <p class="ml-auto text-xs text-gray-300">{{ dateText }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
 import { IChatMessage } from '~/types/messages-client-types';
 
-defineProps<{
+const props = defineProps<{
   message: IChatMessage;
   isMyMessage: boolean;
 }>();
+
+const dateText = computed(() =>
+  new Date(props.message.createdAt).toLocaleDateString()
+);
 </script>
