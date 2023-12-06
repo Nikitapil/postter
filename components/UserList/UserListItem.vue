ec287a41a961d9c87e682475a0908e863e67d5e9
Nikitapil <nikitapiliavets@gmail.com>
2023-12-06 21:48:05 +0300

    add loading state for toggleFollow method in UserListItem

--- components/UserList/UserListItem.vue
+++ components/UserList/UserListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <PreviewCardItem @click="openUser">
-    <div class="flex flex-row justify-between items-center p-2">
-      <div class="flex flex-row">
+    <div class="flex justify-between items-center p-2">
+      <div class="flex">
         <UserAvatar :link="user.profileImage" />
 
         <div class="flex flex-col ml-2">
@@ -14,7 +14,8 @@
 
       <div class="flex h-full">
         <button
-          class="px-4 py-2 font-bold text-xs text-white dark:text-black bg-black dark:bg-white rounded-full hover:opacity-75"
+          class="px-4 py-2 font-bold text-xs text-white dark:text-black bg-black dark:bg-white rounded-full hover:opacity-75 disabled:opacity-50"
+          :disabled="isFollowToggleInProgress"
           @click.stop="onClickFollow"
         >
           {{ followBtnText }}
@@ -43,9 +44,9 @@ const followBtnText = computed(() =>
   props.user.isFollowedByCurrent ? 'Unfollow' : 'Follow'
 );
 
-const onClickFollow = () => {
+const onClickFollow = async () => {
   isFollowToggleInProgress.value = true;
-  toggleFollow(props.user);
+  await toggleFollow(props.user);
   isFollowToggleInProgress.value = false;
 };
 </script>
